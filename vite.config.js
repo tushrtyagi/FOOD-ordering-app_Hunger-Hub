1e67edde47a41ae891089b96dff263c5fafab075
Tushar Tyagi <tyagi121201@gmail.com>
2024-07-03 19:37:29 +0530

    updated config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/foodorder/",
+  base: "/FOOD-ordering-app_Hunger-Hub/",
   plugins: [react()],
 });
